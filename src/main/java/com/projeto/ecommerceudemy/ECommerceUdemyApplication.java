b57681c6706ecaf1a1c9d09766d2887eb8744e74
Felipe Sutter <lipeps61@gmail.com>
2024-01-14 16:28:03 -0300

    initial commit

---
+++ src/main/java/com/projeto/ecommerceudemy/ECommerceUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.ecommerceudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceUdemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/ecommerceudemy/ECommerceUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.ecommerceudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
