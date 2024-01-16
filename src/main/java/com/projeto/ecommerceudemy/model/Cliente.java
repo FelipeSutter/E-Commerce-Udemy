056537174790c176a742d5546be2c2b1ec06dee8
Felipe Sutter <lipeps61@gmail.com>
2024-01-16 11:52:37 -0300

    feat: created Client and Address models

---
+++ src/main/java/com/projeto/ecommerceudemy/model/Cliente.java
@@ -0,0 +1,72 @@
+package com.projeto.ecommerceudemy.model;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Cliente {
+
+    @Id
+    @Column(name = "cliente_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String cpf;
+
+    private String email;
+
+    @OneToOne
+    @JoinColumn(name = "endereco_id", referencedColumnName = "endereco_id", unique = true)
+    @JsonManagedReference
+    private Endereco endereco;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+}
